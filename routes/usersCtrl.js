cf5e0d62e76f5f6db24cf2415e4408fce5d8a8b7
sophie <sophie-girard91@outlook.fr>
2021-08-25 19:39:48 +0200

    creation des logins

---
+++ routes/usersCtrl.js
@@ -0,0 +1,13 @@
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+
+//routes
+
+module.exports = {
+    register: function (req, res) {
+
+    },
+    login: function (req, res) {
+
+    },
+}
--- server.js
+++ server.js
@@ -1,11 +1,23 @@
 const express = require('express');
 const server = express();
+const bodyParser = require('body-parser');
 
+const apiRouter = require('./api-router').router;
+
+//initiali server
+const server = express();
+
+//bodyparser config
+server.use(bodyParser.urlencoded({ extended: true}));
+server.use(bodyParser.json());
+
+//configure routes
 server.get('/', function (req, res) {
     res.setHeader('Content-Type','text/html');
     res.status(200).send('sqlut q tus');
 });
-
+server.use('/api', apiRouter);
+//launch server
 server.listen(3000, function () {
     console.log('server en ceoute')
 });
