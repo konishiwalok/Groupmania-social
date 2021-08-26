6bde762b311dd24e85ec9634d44858443089d1a8
sophie <sophie-girard91@outlook.fr>
2021-08-26 17:19:14 +0200

    pas de acces

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,63 +1,60 @@
 const bcrypt = require("bcryptjs");
-const cryptojs = require("crypto-js");
+const cryptojs = require('crypto-js');
 
 const { generateJWT } = require("../helpers/jwt");
 const User = require("../models/User");
 
 exports.signup = (req, res, next) => {
-  const cryptedResearchedEmail = cryptojs
-    .HmacSHA256(req.body.email, process.env.EMAIL_KEY)
-    .toString();
+  const cryptedResearchedEmail = cryptojs.HmacSHA256(req.body.email, process.env.EMAIL_KEY).toString();
   User.findOne({ email: cryptedResearchedEmail })
-    .then((user) => {
+    .then(user => {
       if (user) {
         return res.status(401).json({
           ok: false,
-          msg: "utilisateur déjà enregistré",
+          msg: 'utilisateur déjà enregistré'
         });
       }
-      bcrypt
-        .hash(req.body.password, 10)
-        .then((hash) => {
+      bcrypt.hash(req.body.password, 10)
+        .then(hash => {
+
           const user = new User({
-            email: cryptojs
-              .HmacSHA256(req.body.email, process.env.EMAIL_KEY)
-              .toString(), // cryptage de l'email, méthode 'HmacSHA256' SANS salage (pour pouvoir ensuite rechercher l'utilisateur simplement lors du login),
-            password: hash,
+            email: cryptojs.HmacSHA256(req.body.email, process.env.EMAIL_KEY).toString(), // cryptage de l'email, méthode 'HmacSHA256' SANS salage (pour pouvoir ensuite rechercher l'utilisateur simplement lors du login),
+            password: hash
           });
-          user
-            .save()
-            .then(() => res.status(201).json({ message: "Utilisateur créé !" }))
-            .catch((error) => res.status(400).json({ error }));
+          user.save()
+            .then(() => res.status(201).json({ message: 'Utilisateur créé !' }))
+            .catch(error => res.status(400).json({ error }));
         })
-        .catch((error) => res.status(500).json({ error }));
+        .catch(error => res.status(500).json({ error }));
     })
-    .catch((error) => res.status(500).json({ error }));
+    .catch(error => res.status(500).json({ error }));
+
+
+
 };
 
 exports.login = (req, res, next) => {
-  const cryptedResearchedEmail = cryptojs
-    .HmacSHA256(req.body.email, process.env.EMAIL_KEY)
-    .toString();
+  const cryptedResearchedEmail = cryptojs.HmacSHA256(req.body.email, process.env.EMAIL_KEY).toString();
   User.findOne({ email: cryptedResearchedEmail })
-    .then((user) => {
+    .then(user => {
       if (!user) {
-        return res.status(401).json({ error: "Utilisateur non trouvé !" });
+        return res.status(401).json({ error: 'Utilisateur non trouvé !' });
       }
-      bcrypt
-        .compare(req.body.password, user.password)
-        .then((valid) => {
+      bcrypt.compare(req.body.password, user.password)
+        .then(valid => {
           if (!valid) {
-            return res.status(401).json({ error: "Mot de passe incorrect !" });
+            return res.status(401).json({ error: 'Mot de passe incorrect !' });
           }
-          generateJWT(user._id).then((token) => {
-            res.status(200).json({
-              userId: user._id,
-              token,
-            });
-          });
+          generateJWT(user._id)
+            .then(token => {
+              res.status(200).json({
+                userId: user._id,
+                token
+              });
+            })
         })
-        .catch((error) => res.status(500).json({ error }));
+        .catch(error => res.status(500).json({ error }));
     })
-    .catch((error) => res.status(500).json({ error }));
+    .catch(error => res.status(500).json({ error }));
 };
+
--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,32 +1,32 @@
 //Path: /api/user
-const { Router } = require("express");
-const { check } = require("express-validator");
+const { Router } = require('express'); 
+const { check } = require('express-validator');
 
-const passwordVerify = require("../middleware/password");
+const passwordVerify = require('../middleware/password');
 
-const userCtrl = require("../controllers/auth.controller");
-const { validator } = require("../middleware/validator");
+const userCtrl = require('../controllers/auth.controller');
+const { validator } = require('../middleware/validator');
 
 const router = Router();
-router.post(
-  "/signup",
+router.post('/signup',
 
-  [
-    check("email", "Le courrier est obligatoire").isEmail(),
-    check("password", "Le mot de passe est obligatoire").not().isEmpty(),
-    validator,
-  ],
-  passwordVerify,
-  userCtrl.signup
+[
+        check('email', 'Le courrier est obligatoire').isEmail(),
+        check('password', 'Le mot de passe est obligatoire').not().isEmpty(),
+        validator
+    ],
+    passwordVerify,
+    userCtrl.signup
+    
 );
-router.post(
-  "/login",
-  [
-    check("email", "Le courrier est obligatoire").isEmail(),
-    check("password", "Le mot de passe est obligatoire").not().isEmpty(),
-    validator,
-  ],
-  userCtrl.login
+router.post('/login',
+    [
+        check('email', 'Le courrier est obligatoire').isEmail(),
+        check('password', 'Le mot de passe est obligatoire').not().isEmpty(),
+        validator
+    ],
+    userCtrl.login
 );
+
 
 module.exports = router;
