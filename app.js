1c9364a6537e7ee739cab5d3409ae8072df38efa
sophie <sophie-girard91@outlook.fr>
2021-08-29 13:18:50 +0200

    routes

--- app.js
+++ app.js
@@ -5,28 +5,25 @@ const path = require('path');
 const helmet = require('helmet');
 const app = express();
 const rateLimit = require("express-rate-limit");
-// const { dbConnection } = require('./database/config');
-
+const { dbConnection } = require('./database/config');
 const limiter = rateLimit({
   windowMs: 15 * 60 * 1000, // 15 minutes
   max: 100 // limit each IP to 100 requests per windowMs
 });
 
-//  apply to all requests
-app.use(limiter);
-
+//CORS CROSS ORIGIN RESEARCH SEARCHING
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
-    next();
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+  next();
 });
 
 const session = require('express-session');
 app.use(session({
-secret:'secret',
-resave: true,
-saveUninitialized:true
+  secret:'secret',
+  resave: true,
+  saveUninitialized:true
 }));
 
 const dbconnection = require('./database/db');
@@ -37,12 +34,18 @@ app.use( express.json());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-
+//images multer
 app.use('/images', express.static(path.join(__dirname, 'images')));
 
-app.use(helmet());
 
-app.use('/api/auth', require('./routes/auth.routes'));
-// Reading and parsing the body
+app.use('/api/post', postsRoutes);
+app.use('/api/users', usersRoutes);
+app.use('/api/post', commentsRoutes);
+
+//securtity 
+app.use(limiter);
+app.use(helmet());
+app.use(rateLimit());
+
 
 module.exports = app;
