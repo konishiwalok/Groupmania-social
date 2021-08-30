9d7bdbcb8fd5461743255629bea7b00e674d311f
sophie <sophie-girard91@outlook.fr>
2021-08-30 14:48:40 +0200

    verifications+db

---
+++ db.config.js
@@ -0,0 +1,50 @@
+const mysql = require('mysql');
+const { Sequelize } = require('sequelize');
+require('dotenv').config();
+
+const sequelize = new Sequelize({
+
+    host: 'DB_HOST',
+    user: 'DB_USERNAME',
+    password: 'DB_PASS',
+    database: 'groupomania_development',
+    dialect: "mysql"
+
+});
+const db = {};
+try {
+    db;
+    console.log('dbconnection is okey!');
+} catch (error) {
+    console.error('Impossible de se connecter, erreur suivante :', error);
+}
+
+db.Sequelize = Sequelize;
+db.sequelize = sequelize;
+
+
+
+
+
+// const mysql = require("mysql");
+// const Sequelize = require("sequelize")
+// require('dotenv').config();
+// const dbConnection = mysql.createConnection({
+//   host: process.env.DB_HOST,
+//   user: process.env.DB_USER,
+//   password: process.env.DB_PASSWORD,
+//   database: process.env.database_development_groupmania ,
+// });
+
+// dbConnection.connect((error) => {
+//   if (error) {
+//     console.log("connection error is :" + error);
+//     return;
+//   }
+//   console.log("dbconnection is okey!");
+// });
+
+// module.exports = dbConnection;
+
+
+// database: process.env.DB_DATABASE,
